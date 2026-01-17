9c91aff3df0c701270563c5ae1d030224424636b
MJ1717 <bananahamo77@gmail.com>
2026-01-16 22:39:51 -0500

    upadted

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,21 +2,30 @@ import express from "express";
 import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB }  from "./config/db.js";
 import dotenv from "dotenv";
+import rateLimiter from "./middleware/rateLimiter.js";
 
 dotenv.config();
 
 const app = express();
 
-connectDB();
+
 
 app.use(express.json());
 
+// app.use((req, res, next) => {
+//   console.log(`${req.method} ${req.url}`);
+//   next();
+// });
+
+app.use(rateLimiter);
+
 app.use("/api/notes", notesRoutes);
 
-
+connectDB().then(() => {
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
+});
 });
 
 
